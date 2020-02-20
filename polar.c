8f6801e0a732663076867eaf374bd2a56ed0f9a4
StevenChiang57 <46946116+StevenChiang57@users.noreply.github.com>
2020-02-19 17:47:40 -0800

    Add files via upload

---
+++ polar.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.>
+
+#define MAXOP 100
+#define NUMBER '0'
+#define MAXVAL 100
+int getop(char[]);
+void push(double);
+double pop(void);
+
+int sp = 0;
+double val[MAXVAL];
+
+
+int main(void){
+  int type;
+  double op2;
+  char s[MAXOP];
+  while ((type = getop(s)) != EOF) {
+    switch (type){
+      case NUMBER:
+        push(atof(s));
+        break;
+      case '+':
+        push(pop() + pop());
+        break;
+      case '*':
+        push(pop() * pop());
+        break;
+      case '-':
+        op2 = pop();
+        push(pop() - op2);
+        break;
+      case '/':
+        op2 = pop();
+        if(op2 != 0.0)
+          push(pop() / op2);
+        else
+          printf("error: zero divisor\n");
+        break;
+      case '\n':
+        printf("\t%.8g\n", pop());
+        break;
+      default:
+        printf("error: unknown command %s\n", s);
+
+    }
+  }
+  return 0;
+}
+
+void push(double f){
+  if (sp < MAXVAL)
+    val[sp++] = f;
+  else
+    prinf("error: stack full, cant push %g\n", f);
+}
+
+double pop(void){
+  if (sp > 0)
+    return val[--sp];
+  else{
+    printf("error: stack empty\n");
+    return 0.0;
+  }
+}
